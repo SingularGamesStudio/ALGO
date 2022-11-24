700004e7ca6a08ccc523531ed9016469140ef57f
eee <kirgrisha04@mail.ru>
2022-11-24 12:47:25 +0300

    init commit

---
+++ FFT2.cpp
@@ -0,0 +1,80 @@
+#include <algorithm>
+#include <iostream>
+#include <vector>
+
+int reversebits(int x, int pw2) {
+    int res = 0;
+    for (int i = 0; i < pw2; i++) {
+        if ((x >> i) & 1)
+            res |= (1 << (pw2 - i - 1));
+    }
+    return res;
+}
+
+class Complex {
+   public:
+    long double r;
+    long double i;
+
+    Complex() : r(0), i(0) {}
+
+    Complex(long double x) : r(x), i(0) {}
+
+    Complex(long double r, long double i) : r(r), i(i) {}
+
+    Complex(const Complex &tocopy) : r(tocopy.r), i(tocopy.i) {}
+
+    Complex operator+(const Complex &second) const {
+        return Complex(r + second.r, i + second.i);
+    }
+
+    Complex operator-(const Complex &second) const {
+        return Complex(r - second.r, i - second.i);
+    }
+
+    Complex operator*(const Complex &second) const {
+        return Complex(r * second.r - i * second.i, i * second.r + r * second.i);
+    }
+
+    Complex &operator*=(const Complex &second) {
+        long double temp = r;
+        r = r * second.r - i * second.i;
+        i = i * second.r + temp * second.i;
+        return *this;
+    }
+
+    Complex &operator=(const Complex &second) {
+        r = second.r;
+        i = second.i;
+        return *this;
+    }
+};
+
+void FFT(Complex *a, int n, Complex q) {
+    int pw2 = 0;
+    while ((1 << pw2) < n)
+        pw2++;
+    for (int i = 0; i < n; i++) {
+        int rev = reversebits(i, pw2);
+        if (i < rev)
+            std::swap(a[i], a[rev]);
+    }
+    for (int l = 2; l <= n; l *= 2) {
+        Complex cur = q;
+        for (int ll = n; ll > l; ll /= 2)
+            cur *= cur;
+        for (int start = 0; start < n; start += l) {
+            int mid = start + l / 2;
+            Complex qdeg = 1;
+            int pos = start;
+            while (pos < mid) {
+                Complex u = Complex(a[pos]);
+                Complex v = Complex(a[pos + l / 2]) * qdeg;
+                a[pos] = u + v;
+                a[pos + l / 2] = u - v;
+                qdeg *= cur;
+                pos++;
+            }
+        }
+    }
+}
